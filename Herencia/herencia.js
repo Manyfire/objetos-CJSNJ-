0d3013ae07e029f903ebc63f47b9427052bcb60a
ORT <ort@many.com>
2023-04-02 23:18:40 -0300

    Hice una nueva contribucion

--- Herencia/herencia.js
+++ Herencia/herencia.js
@@ -1,27 +1,47 @@
-class Personna{
-  constructor(genero,edad,escolaridad,pais,ciudad,estadoCivil){
-    this.genero = genero;
-    this.edad = edad;
-    this.escolaridad = escolaridad;
-    this.pais = pais;
-    this.ciudad = ciudad ;
-    this.estadoCivil = estadoCivil;
-    this.frase = `Soy <b>${genero}</b> y tengo <b>${edad}</b> anos de edad. Ya termine la <b>${escolaridad}</b> en <b>${pais}</b> y  ahora estoy viviendo en la ciudad de <b>${ciudad}</b>. Mi estado civil es <b>${estadoCivil}</b>`;
+// class telephone {
+//   constructor(marca){
+//     this.marca = marca;
+//     this.frase = `Le nouveau telephone <b>${marca}</b> est arrive! `
+//   }
+
+//   verFrase(){
+//     document.write(this.frase + '<br>')
+//   }
+
+// }
+
+// let monTelephone = new telephone('Samsung S6');
+
+// monTelephone.verFrase();
+//VAMOS A VER HERENCIA
+//---------------------
+class Telephone {
+  constructor(marca){
+    this.marca = marca;
+    this.frase = `Le nouveau telephone <b>${marca}</b> est arrive! `
   }
-  
-  
+
   verFrase(){
-    document.write(this.frase +'<br>');
+    document.write(this.frase + '<br>')
+  }
+}
+//HERENCIA:
+//child =>
+class modeloNuevo extends Telephone {
+  constructor(marca,modeloNuevo){
+    super(marca);
+    this.modeloNuevo = modeloNuevo;
+  }
+
+  verFrase2(){
+    document.write(this.verFrase() + 'Le nouveau Model est GOOGL-Pixel' + this.modeloNuevo)
   }
 }
 
 
 
+//let monTelephone = new Telephone('Samsung S6');
+let monTelephone = new Telephone('google Pixel');
 
-let hombre = new Personna('hombre', 34, 'Universidad', 'Argentina', 'Buenos Aires','Soltero' + '<br>');
-let mujer = new Personna('mujer', 18, 'secunadaria', 'Peru', 'Lima','Soltera' + '<br>');
-let lgtb = new Personna('LGTB', 45, ' licenciatura', 'Guatemala', 'Panajachel','Casado' + '<br>');
 
-hombre.verFrase();
-mujer.verFrase();
-lgtb.verFrase();
+monTelephone.verFrase();
