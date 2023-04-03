619b4a50603a1a721c25a8a38e157d3bfe150a3c
ORT <ort@many.com>
2023-04-03 15:43:39 -0300

    add new family-error

--- Herencia/herencia.js
+++ Herencia/herencia.js
@@ -18,7 +18,7 @@
 class Telephone {
   constructor(marca){
     this.marca = marca;
-    this.frase = `Le nouveau telephone <b>${marca}</b> est arrive! `
+    this.frase = `Le telephone <b>${marca}</b> est arrive! `
   }
 
   verFrase(){
@@ -28,20 +28,52 @@ class Telephone {
 //HERENCIA:
 //child =>
 class modeloNuevo extends Telephone {
-  constructor(marca,modeloNuevo){
-    super(marca);
+  constructor(marca, modeloNuevo){
+    super(marca);//palabra reservada"super"
     this.modeloNuevo = modeloNuevo;
+    this.frase = `Mon dernier cellulaire: <b>${marca}</b> est brisé la semaine dèrniere Mais aujourd'hui est arrivé mon nouveux cellulaire: <b>${modeloNuevo}</b>`
   }
 
   verFrase2(){
-    document.write(this.verFrase() + 'Le nouveau Model est GOOGL-Pixel' + this.modeloNuevo)
+    document.write(this.verFrase() + this.modeloNuevo())
   }
 }
 
+let monTelephone = new modeloNuevo('Samsung S6','iphone 12');
+document.write(monTelephone.verFrase2());
+//---------------------------------------------------------------
+//window.addEventListener('load', function(){//load:hacta que no se carge la pagina no va ejecutarla.
+  class family {
+    constructor(dady,mamy,brother) {
+      this.dady = dady;
+      this.mamy = mamy;
+      this.brother = brother;
+      this.frase = `This is the beauty family ${this.dady},${this.mamy},${this.brother}`
+    }
+    frases(){
+      document.write(this.frase + '<br>')
+      //return `This is the beauty family ${dady},${mamy},${brother}`//error this.
+    }
+  }
+  //Classe Hereda => super()
+  class newFamily extends family{
+    constructor(dady,mamy,brother,newFamily){
+      super(dady,mamy,brother,newFamily);
+      this.newFamily = newFamily;
+      this.frase =`mi family is:${dady},${mamy},${brother},${newFamily}`
+    }
+    fraseComply(){
+      document.write(this.frases() + this.newFamily())
+      //return this.frese() + 'a new person came to the new family' + this.newFamily
+    }
+  }
 
+  let newPerson = new newFamily('George','Melanie','john','Isabelle')
+  document.write(newPerson.fraseComply())
+  //frase.innerHTML = newPerson.fraseComply()
 
-//let monTelephone = new Telephone('Samsung S6');
-let monTelephone = new Telephone('google Pixel');
+//},true)
 
-
-monTelephone.verFrase();
+/*
+para hacer herencia se necesita la palabra reservada: "EXTENDS"
+La classe HERENCIA va tomar todos los metodos de la classe padre*/
